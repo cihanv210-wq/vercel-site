799651897a99010befa5fd3f7b960749c96a9077
cihanv210-wq <cihanv21.0@gmail.com>
2026-01-16 09:27:16 +0300

    Edit4

--- script.js
+++ script.js
@@ -11,21 +11,20 @@ function loadPage(page) {
 
             <div class="form-group">
                 <label>Ad Soyad</label>
-                <input id="musteriAd" />
+                <input id="musteriAd">
             </div>
 
             <div class="form-group">
                 <label>Telefon</label>
-                <input id="musteriTel" />
+                <input id="musteriTel">
             </div>
 
             <div class="form-group">
                 <label>Adres</label>
-                <input id="musteriAdres" />
+                <input id="musteriAdres">
             </div>
 
             <button onclick="musteriEkle()">Ekle</button>
-
             <div id="musteriListesi"></div>
         `;
         renderMusteriler();
@@ -37,16 +36,15 @@ function loadPage(page) {
 
             <div class="form-group">
                 <label>Hizmet İsmi</label>
-                <input id="hizmetAd" />
+                <input id="hizmetAd">
             </div>
 
             <div class="form-group">
                 <label>Fiyat</label>
-                <input id="hizmetFiyat" />
+                <input id="hizmetFiyat">
             </div>
 
             <button onclick="hizmetEkle()">Ekle</button>
-
             <div id="hizmetListesi"></div>
         `;
         renderHizmetler();
@@ -58,36 +56,30 @@ function loadPage(page) {
 
             <div class="form-group">
                 <label>Ürün İsmi</label>
-                <input id="urunAd" />
+                <input id="urunAd">
             </div>
 
             <div class="form-group">
                 <label>Fiyat</label>
-                <input id="urunFiyat" />
+                <input id="urunFiyat">
             </div>
 
             <button onclick="urunEkle()">Ekle</button>
-
             <div id="urunListesi"></div>
         `;
         renderUrunler();
     }
 }
 
-/* MÜŞTERİ */
+/* EKLE / SİL */
 function musteriEkle() {
-    musteriler.push({
-        ad: musteriAd.value,
-        tel: musteriTel.value,
-        adres: musteriAdres.value
-    });
+    musteriler.push({ ad: musteriAd.value, tel: musteriTel.value });
     renderMusteriler();
 }
 
 function renderMusteriler() {
     const div = document.getElementById("musteriListesi");
     if (!div) return;
-
     div.innerHTML = "";
     musteriler.forEach((m, i) => {
         div.innerHTML += `
@@ -99,61 +91,14 @@ function renderMusteriler() {
     });
 }
 
-/* HİZMET */
 function hizmetEkle() {
-    hizmetler.push({
-        ad: hizmetAd.value,
-        fiyat: hizmetFiyat.value
-    });
+    hizmetler.push({ ad: hizmetAd.value, fiyat: hizmetFiyat.value });
     renderHizmetler();
 }
 
 function renderHizmetler() {
     const div = document.getElementById("hizmetListesi");
     if (!div) return;
-
     div.innerHTML = "";
     hizmetler.forEach((h, i) => {
-        div.innerHTML += `
-            <div class="list-item">
-                ${h.ad} - ${h.fiyat} ₺
-                <button onclick="hizmetler.splice(${i},1); renderHizmetler()">Sil</button>
-            </div>
-        `;
-    });
-}
-
-/* ÜRÜN */
-function urunEkle() {
-    urunler.push({
-        ad: urunAd.value,
-        fiyat: urunFiyat.value
-    });
-    renderUrunler();
-}
-
-function renderUrunler() {
-    const div = document.getElementById("urunListesi");
-    if (!div) return;
-
-    div.innerHTML = "";
-    urunler.forEach((u, i) => {
-        div.innerHTML += `
-            <div class="list-item">
-                ${u.ad} - ${u.fiyat} ₺
-                <button onclick="urunler.splice(${i},1); renderUrunler()">Sil</button>
-            </div>
-        `;
-    });
-}
-
-/* TARİH & SAAT */
-function updateDateTime() {
-    const now = new Date();
-    document.getElementById("datetime").innerHTML =
-        now.toLocaleDateString("tr-TR") + "<br>" +
-        now.toLocaleTimeString("tr-TR");
-}
-
-setInterval(updateDateTime, 1000);
-updateDateTime();
+        div.innerHTML
